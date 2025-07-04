3234ff8ba6aa3322b5740766627a8988bb8d0c3c
Jigoooo <jiwook97@gmail.com>
2025-07-04 10:25:14 +0900

    앵커 추가

--- packages/core/src/ui/picker/ui/anchor-without-action.tsx
+++ packages/core/src/ui/picker/ui/anchor-without-action.tsx
@@ -64,7 +64,7 @@ export function AnchorWithoutAction({
   updateRef.current = update;
 
   useEffect(() => {
-    if (!forceUpdateTrigger && isOpen && updateRef.current) {
+    if (!!forceUpdateTrigger && isOpen && updateRef.current) {
       requestAnimationFrame(() => {
         updateRef.current?.();
       });
