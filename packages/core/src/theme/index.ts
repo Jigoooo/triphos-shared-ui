f2ce637cb0f371f9d281ca760b3c3bf1924bb9ba
Jigoooo <jiwook97@gmail.com>
2025-08-01 14:22:11 +0900

    커스텀 색상 추가

--- packages/core/src/theme/index.ts
+++ packages/core/src/theme/index.ts
@@ -1,7 +1,7 @@
 export { themeBase } from './config/theme-base.ts';
 
 export { useThemeContext } from './model/theme-context.tsx';
-export type { ThemeContextType, Theme, ThemeInput, CustomColorValue } from './model/theme-type.ts';
+export type { ThemeContextType, Theme, ThemeInput } from './model/theme-type.ts';
 export { ThemeProvider } from './model/theme-provider.tsx';
 
 export { createTheme } from './lib/create-theme.ts';
