dde44d4ee3ff161800d417d1d423864918fb8616
Jigoooo <jiwook97@gmail.com>
2025-08-28 16:42:19 +0900

    bottomsheet 닫힘 동작 수정

--- packages/core/src/ui/bottom-sheet/index.ts
+++ packages/core/src/ui/bottom-sheet/index.ts
@@ -4,4 +4,3 @@ export { BottomSheetProvider } from './ui/bottom-sheet-provider.tsx';
 export { useBottomSheet } from './model/bottom-sheet-context.ts';
 
 export type { BottomSheetProps, BottomSheetWithInputProps } from './model/bottom-sheet-type.ts';
-export type { BottomSheetConfig, BottomSheetRenderProps } from './model/bottom-sheet-context.ts';
