f67cc2c18e4b7ec6ebbbb47c18d315205316cc24
Jigoooo <jiwook97@gmail.com>
2025-08-31 15:26:12 +0900

    폰트 삭제

--- packages/core/src/theme/model/theme-provider.tsx
+++ packages/core/src/theme/model/theme-provider.tsx
@@ -1,6 +1,5 @@
 import '../../../public/css/index.css';
 import '../../../public/css/code.css';
-import '../../../public/css/font.css';
 import '../../../public/css/loader.css';
 
 import type { ReactNode } from 'react';
